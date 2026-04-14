8484abe903935a614b386e8329c5259c1a405512
Keerthana <241210059@nitdelhi.ac.in>
2026-04-14 18:37:20 +0530

    Removed duplicate root files

--- ml/forecaster.py
+++
@@ -1,6 +0,0 @@
-def generate_forecast(sales, current_stock, forecast_days, state):
-    # change this with your actual ml code, I just put it in temporarily
-    predicted_demand = 0
-    suggested_reorder = 0
-    confidence = 0.0
-    return predicted_demand, suggested_reorder, confidence
